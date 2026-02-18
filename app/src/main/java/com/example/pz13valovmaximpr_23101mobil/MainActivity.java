8da4393b9e498c38ad749464f6fcdd5c19f3a69a
MAX0YT <valov.maks.2008@gmail.com>
2026-02-18 17:08:56 +0700

    UI added

--- app/src/main/java/com/example/pz13valovmaximpr_23101mobil/MainActivity.java
+++ app/src/main/java/com/example/pz13valovmaximpr_23101mobil/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.pz13valovmaximpr_23101mobil;
 
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +18,45 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e) {}
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        ((Button)findViewById(R.id.btn_start)).setBackgroundTintList(ColorStateList.valueOf(Color.BLUE));
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public boolean Started = false;
+    public boolean Finished = false;
+    public void Start(View view){
+        Button button = (Button)findViewById(R.id.btn_start);
+        if(!Finished){
+            if(!Started){
+                button.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
+                button.setText("Пауза");
+                Started = true;
+            }
+            else if(Started){
+                button.setBackgroundTintList(ColorStateList.valueOf(Color.BLUE));
+                button.setText("Старт");
+                Started = false;
+            }
+        }
+        else{
+            Intent intent = new Intent(MainActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+    public void Drive1(View view){
+        Button button = (Button)findViewById(R.id.btn_green);
+    }
+    public void Drive2(View view){
+        Button button = (Button)findViewById(R.id.btn_green);
     }
 }
